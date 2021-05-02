2fb0ee74538cd24c9d5adfef587901378a0cc9da
Lucas Hermont <lucas.hermont@hotmail.com>
2021-05-02 14:52:46 -0300

    Iniciando projeto da api

---
+++ src/main/java/lucasti/viavarejo/ViavarejoApplication.java
@@ -0,0 +1,13 @@
+package lucasti.viavarejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViavarejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViavarejoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lucasti/viavarejo/ViavarejoApplicationTests.java
@@ -0,0 +1,13 @@
+package lucasti.viavarejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViavarejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
