4437961d715642d69df7520e8ee08b70cd4d6795
Lucas Hermont <lucas.hermont@hotmail.com>
2021-05-02 18:21:09 -0300

    Corrigindo nome do servico de busca e de deletar

--- src/main/java/lucasti/viavarejo/services/ApoliceService.java
+++ src/main/java/lucasti/viavarejo/services/ApoliceService.java
@@ -9,12 +9,12 @@ public interface ApoliceService {
 	
 	public List<Apolice> findAll();
 	
-	public Apolice findById(String id);
+	public Apolice findByNumero(String id);
 	
 	public Apolice save(Apolice apolice);
 	
 	public Apolice update(Apolice apolice, String id);
 	
-	public void deleteById(String id);
+	public void deleteByNumero(String id);
 
 }
