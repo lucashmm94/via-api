99718b45ebe48b2957f08af6581e99cc2f983d6d
Lucas Hermont <lucas.hermont@hotmail.com>
2021-05-03 00:07:35 -0300

    Adicionando validação de cpf do cliente

--- src/main/java/lucasti/viavarejo/models/entities/Cliente.java
+++ src/main/java/lucasti/viavarejo/models/entities/Cliente.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.br.CPF;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -18,6 +19,7 @@ public class Cliente implements Serializable {
 	@NotEmpty(message = "Nome não pode nulo ou branco")
 	private String nome;
 	
+	@CPF(message="CPF inválido")
 	@NotEmpty(message = "CPF não pode nulo ou branco")
 	private String cpf;
 	
