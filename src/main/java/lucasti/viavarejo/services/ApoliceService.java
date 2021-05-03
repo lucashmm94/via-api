4d94907941cefccb54cbac8660dc251de429f58a
Lucas Hermont <lucas.hermont@hotmail.com>
2021-05-03 02:16:47 -0300

    Comentando/documentando services

--- src/main/java/lucasti/viavarejo/services/ApoliceService.java
+++ src/main/java/lucasti/viavarejo/services/ApoliceService.java
@@ -8,13 +8,55 @@ import lucasti.viavarejo.models.entities.Apolice;
 
 public interface ApoliceService {
 	
+	
+	/*
+	 * Lista todas as Apolices
+	 * 
+	 * @return A lista de todas as Apolices.
+	 * 
+	 */
 	public List<Apolice> findAll();
+	
+	/*
+	 * Retorna uma nova Apolice.
+	 * 
+	 * @param numero, .
+	 * 
+	 * @return A Apolice que foi cadastrada.
+	 * 
+	 */
 	
 	public ApoliceDTO findByNumero(String numero);
 	
+	
+	/*
+	 * Cadastra uma nova Apolice.
+	 * 
+	 * @param Apolice, a apolice que vai ser cadastrada.
+	 * 
+	 * @return A Apolice que foi cadastrada.
+	 * 
+	 */
 	public Apolice save(Apolice apolice);
 	
+	
+	/*
+	 * Retorna a apolice.
+	 * 
+	 * @param Apolice, A Apolice que será atualizada.
+	 * 
+	 * @param numero, O numero(id) da Apolice que será atualizada.
+	 * 
+	 * @return A Apolice que foi atualizada.
+	 * 
+	 */
 	public Apolice update(Apolice apolice, String id);
+	
+	/*
+	 * Exclui uma Apolice pelo Numero
+	 * 
+	 * @param numero, numero(id) da Apolice que será deletada.
+	 */
 	
 	public void deleteByNumero(String numero);
 
--- src/main/java/lucasti/viavarejo/services/ClienteService.java
+++ src/main/java/lucasti/viavarejo/services/ClienteService.java
@@ -7,14 +7,57 @@ import lucasti.viavarejo.models.entities.Cliente;
 
 public interface ClienteService {
 	
+	
+	/*
+	 * Retorna todos clientes
+	 * 
+	 * @return A lista de todos os clientes.
+	 * 
+	 */
 	public List<Cliente> findAll();
 	
+	
+	/*
+	 * Pesquisa Cliente pelo ID.
+	 * 
+	 * @param id, O id do Cliente que será pesquisado.
+	 * 
+	 * @return O Cliente que foi pesquisado.
+	 * 
+	 */
 	public Cliente findById(String id);
 	
+	
+	/*
+	 * Salva um novo Cliente.
+	 * 
+	 * @param Cliente, O Cliente que será criado.
+	 * 
+	 * @return O Cliente que foi salvado.
+	 * 
+	 */
 	public Cliente save(Cliente cliente);
 	
+	
+	/*
+	 * Atualiza um Cliente.
+	 * 
+	 * @param Cliente, O Cliente que será atualizado.
+	 * 
+	 * @param id, O ID do Cliente que será atualizada.
+	 * 
+	 * @return O Cliente que foi atualizado.
+	 * 
+	 */
 	public Cliente update(Cliente cliente, String id);
 	
+	
+	/*
+	 * Exclui um Cliente pelo ID
+	 * 
+	 * @param ID, Id do cliente que sera exlcuido.
+	 * 
+	 */
 	public void deleteById(String id);
 
 }
